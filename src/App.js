7cf6c4d10eaf6d0342d5f9298bf32a667049b002
OngakuKun <marcellehmkuhl@outlook.de>
2025-08-29 17:48:15 +0200

    Setup commit

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import './App.css';
+
+const quests = [
+    { type: 'Coop Quest', name: 'Woods of Recollection', weak: 'Electric', res: 'Wind' },
+    { type: 'Coop Quest', name: 'Abandoned Facility', weak: 'Water', res: 'Fire' },
+    { type: 'Coop Quest', name: 'Jade Maze', weak: 'Light', res: 'Dark' },
+    { type: 'Coop Quest', name: 'Ruins of Truth and Lies', weak: 'Wind', res: 'Electric' },
+    { type: 'Coop Quest', name: 'Woodland of Vitality', weak: 'Fire', res: 'Water' },
+    { type: 'Coop Quest', name: 'Amber Maze', weak: 'Dark', res: 'Light' },
+    { type: 'Coop Quest', name: 'Woods of Pluto', weak: 'Light', res: 'Dark' },
+    { type: 'Coop Quest', name: 'Toxic Ruins', weak: 'Wind', res: 'Electric' },
+    { type: 'Coop Quest', name: 'Poisoned Facility', weak: 'Fire', res: 'Water' },
+    { type: 'Boss Raid', name: 'Skull Reaper', weak: 'Light', res: 'Dark' },
+    { type: 'Boss Raid', name: 'SBC Trommel', weak: 'Electric', res: 'Wind' },
+    { type: 'Boss Raid', name: 'Kraken the Abyss Lord', weak: 'Fire', res: 'Water' },
+    { type: 'Boss Raid', name: 'Sword Golem', weak: 'Dark', res: 'Light' },
+    { type: 'Boss Raid', name: 'Dorz\'l the Chaos Drake', weak: 'Water', res: 'Fire' },
+    { type: 'Boss Raid', name: 'Fuscus the Vacant Colossus', weak: 'Wind', res: 'Electric' },
+]
+
+function ExtractQuests({questType}) {
+    return (
+        <div>
+            <h2 className='quest-type-header'> --- {questType} --- </h2>
+            {quests
+                .filter((quest) => quest.type === questType)
+                .map((quest, index) => (
+                    <div key={index} className='quest-card'>
+                        <h3>{quest.name}</h3>
+                        <p>Weak: {quest.weak}</p>
+                        <p>Resistence: {quest.res} </p>
+                    </div>
+                ))}
+        </div>
+    )
+}
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src="https://p325k7wa.twic.pics/high/sword-art-online/sao-fractured-daydream/00-page-setup/saofd-logo.png?twic=v1" className="App-logo" alt="logo" />
+                <h2>Sword Art Online: Fractured Daydream Wiki</h2>
+            </header>
+            <main>
+                <ExtractQuests questType='Coop Quest' />
+                <ExtractQuests questType='Boss Raid' />
+            </main>
+            <footer>
+                Made with GithubPages and React
+            </footer>
+        </div>
+    );
+}
+
+export default App;
