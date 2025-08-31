73de77e00d1540e2c0b9580484ddd9f868f7c140
OngakuKun <marcellehmkuhl@outlook.de>
2025-09-01 01:00:07 +0200

    Add specialeffect translation

--- src/App.js
+++ src/App.js
@@ -172,7 +172,7 @@ function App() {
                         {specialEffects.map((piece, i) => (
                             <ExtractSpecialEffects
                                 key={piece.type + i}
-                                title={`SpecialEffects - ${piece.type}`}
+                                title={"SpecialEffects - " + getTranslation(data.types[piece.type], language)}
                                 effectList={piece.entries}
                                 language={language}
                             />
